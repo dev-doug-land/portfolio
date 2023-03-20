90c45518394dd52ad11e913b1f4b7d65d51390b4
Douglas A <douglassan1@hotmail.com>
2023-03-19 22:12:22 -0300

    Created welcome screen

--- resources/js/Pages/Demo/Index.vue
+++ resources/js/Pages/Demo/Index.vue
@@ -3,12 +3,37 @@
 </script>
 
 <template>
-    <h1 class="text-1xl font-bold underline">
-        Hello world!
-    </h1>
+    <section>
+        <h1 class="text">Welcome To My Portfolio</h1>
+    </section>
 </template>
 
-<style>
+<style lang="scss">
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap');
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+        section{
+            position: relative;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #0e1623;
+            overflow: hidden;
+            .text{
+                position: relative;
+                font-size: 5rem;
+                text-align: center;
+                color: #00fe77;
+            }
+        }
+    }
+
 
 </style>
 
+    
