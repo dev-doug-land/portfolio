4032eec1b9ffbb5e39e2d20fce3009b34080a9cc
Douglas A <douglassan1@hotmail.com>
2023-03-19 23:21:09 -0300

    Test: animejs to work

--- resources/js/Pages/Demo/Index.vue
+++ resources/js/Pages/Demo/Index.vue
@@ -1,10 +1,21 @@
 <script setup>
+    import anime from 'animejs/lib/anime.es.js';
+    let message = 'Welcome To My Portfolio';
 
+    message = message.replace(/\S/g, "<span>$&</span>");    
+    anime.timeline({
+        loop: true,
+    })
+
+    .add({
+        targets: '.text span',
+        translateY: [-600, 0],
+    })
 </script>
 
 <template>
     <section>
-        <h1 class="text">Welcome To My Portfolio</h1>
+        <h1 class="text" v-html="message"></h1>
     </section>
 </template>
 
@@ -29,6 +40,11 @@
                 font-size: 5rem;
                 text-align: center;
                 color: #00fe77;
+                -webkit-box-reflect: below -3.6rem linear-gradient(transparent, #0004);
+                span{
+                    position: relative;
+                    display: inline-block;
+                }
             }
         }
     }
