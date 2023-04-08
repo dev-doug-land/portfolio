bd9d47761d439104fb1e02ede53496a45f3c9fcc
Douglas A <douglassan1@hotmail.com>
2023-04-07 23:43:05 -0300

    Test: changing position absolute

--- resources/js/Pages/Components/Compass.vue
+++ resources/js/Pages/Components/Compass.vue
@@ -1,32 +1,84 @@
 <script setup>
   import anime from 'animejs';
-  import { onMounted } from 'vue';
+  import { onMounted, ref} from 'vue';
+
+
+  function followMouseCursor(){
+    let box = document.querySelector("#needle");
+    let boxBoundingRect = box.getBoundingClientRect();
+    let boxCenter= {
+        x: boxBoundingRect.left + boxBoundingRect.width/2, 
+        y: boxBoundingRect.top + boxBoundingRect.height/2
+    };
+
+    document.addEventListener("mousemove", e => {
+        let angle = Math.atan2(e.pageX - boxCenter.x, - (e.pageY - boxCenter.y) )*(180 / Math.PI);      
+        box.style.transform = `rotate(${angle}deg)`;  
+    })
+  }
+
+  // function showInConsole(e){
+  //     // anime({
+  //     //   targets: '#compass',
+  //     //   rotate: 5,
+  //     //   scale: 1,
+  //     //   loop: true,
+  //     // });
+  //     setTimeout(() => {
+  //         console.log(e.clientX, e.clientY);
+          
+  //     }, 2000);
+  // }
 
   onMounted(() => {
-    anime({
-      targets: '#compass',
-      rotate: 5,
-      scale: 1,
-      loop: true,
-    });
+    followMouseCursor();
   })
 </script>
 
 <template>
-  <div class="flex justify-center items-center">
+  <div id="background" class="flex justify-center items-center">
     <img id="compass" src="images/compass.svg" alt="Compass image">
+    <div id="needle" class="">
+      <!-- i need create the needle with css border -->
+      <div id="red-tip"></div>
+      <div id="black-tip"></div>
+    </div>
   </div>
 </template>
 
 
 
 <style lang="scss" scoped>
-    #compass{
-      max-width: 100%;
-      max-height: 100%;
-      border: 0.15rem solid black;
-      border-radius: 50%;
-      padding: 1rem;
-      background-color: #fff;
+    #background{
+      position: relative;
+      margin: 0;
+      padding: 0;
+      #compass{
+        max-width: 12rem;
+        max-height: 12rem;
+        border: 0.15rem solid black;
+        border-radius: 50%;
+        padding: 1rem;
+        background-color: #fff;
+      }
+      #needle{
+        position: absolute;
+        left: calc(12rem / 2 - 0.4rem);
+        top: 1rem;
+        #red-tip{
+          width: 0;
+          height: 0;
+          border-left: 0.6rem solid transparent;
+          border-right: 0.6rem solid transparent;
+          border-bottom: 5rem solid #b2140f;
+        };
+        #black-tip{
+          width: 0;
+          height: 0;
+          border-left: 0.6rem solid transparent;
+          border-right: 0.6rem solid transparent;
+          border-top: 5rem solid #343434;
+        }
+      }
     }
 </style>
--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -1,28 +1,35 @@
 <script setup>
+import { Link } from '@inertiajs/vue3';
 import Compass from '../Components/Compass.vue';
+
+// // watch works directly on a ref
+// watch(question, async (newQuestion, oldQuestion) => {
+//   if (newQuestion.indexOf('?') > -1) {
+//     try {
+//       const res = await fetch('https://yesno.wtf/api')
+//       answer.value = (await res.json()).answer
+//     } catch (error) {
+//       answer.value = 'Error! Could not reach the API. ' + error
+//     }
+//   }
+// })
 
 </script>
 
 <template>
-    <div id="background" class="grid h-screen">
-        <div id="option1" class="grid col-span-12 h-[calc(100vh/3)] text-6xl text-[#FFEDE6] ease-in duration-300 hover:text-[#B4F58E] place-content-center">About me</div>
-        <div id="option2" class="grid col-span-5 h-[calc(100vh/3)] text-6xl text-[#FFEDE6] ease-in duration-300 hover:text-[#9AD3F5] place-content-center">Projects</div>
-        <Compass class="col-span-2 h-[calc(100vh/3)]"/>   
-        <div id="option3" class="grid col-span-5 h-[calc(100vh/3)] text-6xl text-[#FFEDE6] ease-in duration-300 hover:text-[#F5DEBF] place-content-center">Skills</div>
-        <div id="option4" class="grid col-span-12 h-[calc(100vh/3)] text-6xl text-[#FFEDE6] ease-in duration-300 hover:text-[#f4a6f5] place-content-center">
-            Contact
+    <div class="grid h-screen">
+        <Link id="option1" :href="'/home'" class="grid col-span-12 h-[calc(100vh/3)] lg:text-6xl md:text-4x1 text-[#FFEDE6] ease-in duration-300 hover:text-[#B4F58E] place-content-center">About me</Link>
+        <div class="flex justify-around w-[100vw]">
+            <Link id="option2" :href="'/home'" class="grid h-[calc(100vh/3)] lg:text-6xl md:text-4x1 text-[#FFEDE6] ease-in duration-300 hover:text-[#9AD3F5] place-content-center">Projects</Link>
+            <Compass class="grid px-4 h-[calc(100vh/3)]"/>   
+            <Link id="option3" :href="'/home'" class="grid h-[calc(100vh/3)] lg:text-6xl md:text-4x1 text-[#FFEDE6] ease-in duration-300 hover:text-[#F5DEBF] place-content-center">Skills</Link>
         </div>
+        <Link id="option4" :href="'/home'" class="grid col-span-12 h-[calc(100vh/3)] lg:text-6xl md:text-4x1 text-[#FFEDE6] ease-in duration-300 hover:text-[#f4a6f5] place-content-center">Contact</Link>
     </div>
 </template>
 
 
 <style lang="scss" scoped>
 
-    #background{
-        div{
-            border: 1px solid white;
-        }
-        width: 100%;
-    }
 
 </style>
